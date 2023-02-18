b030274268fad57d59fb1ca2a3771e2e5da2aaf8
Valk Dokk <91455763+TheValkDokk@users.noreply.github.com>
2023-02-18 19:27:43 +0700

    fix value

--- lib/app/controller/app_controller.dart
+++ lib/app/controller/app_controller.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
@@ -8,8 +7,8 @@ class AppController extends GetxController {
   static AppController instance = Get.find();
 
   GoogleProvider googleProvider = GoogleProvider(
-    clientId: dotenv.env[
-        '345963871008-qph34avag3hakk9ffich62ihcia3834c.apps.googleusercontent.com']!,
+    clientId:
+        '345963871008-qph34avag3hakk9ffich62ihcia3834c.apps.googleusercontent.com',
   );
 
   RxBool isDark = false.obs;
